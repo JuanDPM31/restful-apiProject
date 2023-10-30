967a65342ac075adc16c0d9eabcc099365dd6524
Juan David <jpaz89163@gmail.com>
2023-10-29 22:51:57 -0500

    Go back

--- services/userServices.js
+++ services/userServices.js
@@ -35,8 +35,7 @@ const updateUser = async (id,name,email,password) =>{
                 id,
             }
         })
-        let users = await db.User.findAll()
-        return users
+        return updateUser
     } catch (error) {
         throw {status:500, message: error.message || 'User could not be updated'}
     }
